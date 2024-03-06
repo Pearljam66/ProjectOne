38e9e14c30db1caa3caeba95b7a91f6a19eb7aa1
Sarah Clark <sarahireneclark@gmail.com>
2024-03-06 12:25:53 -0600

    Added table view code

---
+++ ProjectOne/ProjectOne/TableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ProjectOne
+//
+//  Created by Sarah Clark on 3/1/24.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = pictures[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+}
+
